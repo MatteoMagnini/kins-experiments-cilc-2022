58b8f9967b0e8a3184a94985ac264095af805396
Matteo Magnini <matteo.magnini@studio.unibo.it>
2022-04-22 11:35:30 +0200

    fix: refactoring

--- resources/data/__init__.py
+++ resources/data/__init__.py
@@ -1,5 +1,7 @@
 import re
+from itertools import chain
 from pathlib import Path
+from typing import Iterable
 import pandas as pd
 
 PATH = Path(__file__).parents[0]
@@ -8,10 +10,52 @@ PATH = Path(__file__).parents[0]
 def get_data(filename: str) -> pd.DataFrame:
     x = []
     with open(str(PATH / filename) + '.txt') as file:
-        for raw in file:
-            raw = re.sub('\n', '', raw)
-            label, _, features = raw.split(',')
+        for row in file:
+            row = re.sub('\n', '', row)
+            label, _, features = row.split(',')
             features = list(f for f in features.lower())
-            features.append(label)
+            features.append(label.lower())
             x.append(features)
     return pd.DataFrame(x)
+
+
+def get_binary_data(data: pd.DataFrame, mapping: dict[str: set[str]]) -> pd.DataFrame:
+    sub_features = sorted(_get_values(mapping))
+    results = []
+    for _, row in data.iterrows():
+        row_result = []
+        for value in row:
+            positive_features = mapping[value]
+            for feature in sub_features:
+                row_result.append(1 if feature in positive_features else 0)
+        results.append(row_result)
+    return pd.DataFrame(results, dtype=int)
+
+
+def _get_values(mapping: dict[str: set[str]]) -> Iterable[str]:
+    result = set()
+    for values_set in mapping.values():
+        for value in values_set:
+            result.add(value)
+    return result
+
+
+def data_to_int(data: pd.DataFrame, mapping: dict[str: int]) -> pd.DataFrame:
+    return data.applymap(lambda x: mapping[x] if x in mapping.keys() else x)
+
+
+def get_indices() -> list[int]:
+    return list(range(-30, 0)) + list(range(1, 31))
+
+
+def get_feature_mapping(variable_indices: list[int] = get_indices()) -> dict[str: int]:
+    return {'X' + ('_' if j < 0 else '') + str(abs(j)): i for i, j in enumerate(variable_indices)}
+
+
+def get_vocabulary(data: pd.DataFrame) -> list[str]:
+    result = set()
+    for _, row in data.iterrows():
+        for value in row:
+            result.add(value)
+    return sorted(result)
+
--- resources/rules/__init__.py
+++ resources/rules/__init__.py
@@ -2,17 +2,26 @@ import re
 from pathlib import Path
 from typing import Iterable
 
+from resources.data import get_indices
+
 PATH = Path(__file__).parents[0]
 ALPHABET = ('A', 'C', 'G', 'T')
 RULE_DEFINITION_SYMBOLS = ('::-', ':-')
 RULE_DEFINITION_SYMBOLS_REGEX = '(' + '|'.join(RULE_DEFINITION_SYMBOLS) + ')'
 INDEX_IDENTIFIER = '@'
-AGGREGATE_SYMBOLS = {'Y':('C', 'T')}
+AGGREGATE_SYMBOLS = {'Y': ('C', 'T'),
+                     'M': ('A', 'C'),
+                     'R': ('A', 'G')}
+AGGREGATE_DATA_SYMBOLS = {'A': ('A', 'D', 'N', 'R'),
+                          'C': ('C', 'N', 'S'),
+                          'G': ('G', 'D', 'N', 'S', 'R'),
+                          'T': ('T', 'D', 'N')}
 NOT_IDENTIFIER = 'not'
 VARIABLE_BASE_NAME = 'X'
 AND_SYMBOL = ' ∧ '
+OR_SYMBOL = ' ∨ '
 NOT_SYMBOL = '¬'
-LESS_SYMBOL = ' < '
+LESS_EQUAL_SYMBOL = ' ≤ '
 PLUS_SYMBOL = ' + '
 IMPLICATION_SYMBOL = ' ← '
 
@@ -27,23 +36,52 @@ def get_rules(filename: str) -> list[str]:
     return rules
 
 
-def get_datalog_rules(rules: Iterable[str], labels: set[str] = ('ei', 'ie')) -> Iterable[str]:
+def get_datalog_rules(rules: Iterable[str], class_labels: set[str] = ('ei', 'ie', 'n')) -> Iterable[str]:
     results = []
 
     for rule in rules:
         rule = re.sub(r' |\.', '', rule)
         name, _, rest = re.split(RULE_DEFINITION_SYMBOLS_REGEX, rule)
         name = re.sub('-', '_', name.lower())
-        if name in labels:
+        if name in class_labels:
             name = 'class(' + name + ')'
         else:
             name = name + '(' + ')'
-        # lhs = name + '(' + ','.join(VARIABLE_BASE_NAME + ('_' if i < 0 else '') + str(abs(i)) for i in indices) + ')'
         lhs = name
         rhs = _parse_clause(rest)
         results.append(lhs + IMPLICATION_SYMBOL + rhs)
 
     return results
+
+
+def get_rules_with_explicit_variables(rules: Iterable[str]) -> Iterable[str]:
+    result = []
+    regex = r'X[_]?[0-9]* = '
+
+    for rule in rules:
+        for var in AGGREGATE_DATA_SYMBOLS:
+            tmp_rule = rule
+            partial_result = ''
+            while re.search(regex + var.lower(), tmp_rule) is not None:
+                match = re.search(regex + var.lower(), tmp_rule)
+                ante = tmp_rule[:match.regs[0][0]]
+                medio = _replace(var, tmp_rule[match.regs[0][0]:match.regs[0][1]])
+                partial_result += ante + medio
+                tmp_rule = tmp_rule[match.regs[0][1]:]
+            partial_result += tmp_rule
+            rule = partial_result
+        result.append(rule)
+
+    return result
+
+
+def get_rules_with_int(rules: Iterable[str], mapping: dict[str: int]) -> Iterable[str]:
+    result = []
+    for rule in rules:
+        for key in mapping.keys():
+            rule = re.sub(' ' + key, ' ' + str(mapping[key]), rule)
+        result.append(rule)
+    return result
 
 
 def _parse_clause(rest: str, rhs: str = '', aggregation: str = AND_SYMBOL) -> str:
@@ -56,8 +94,10 @@ def _parse_clause(rest: str, rhs: str = '', aggregation: str = AND_SYMBOL) -> st
             clause = clause[len(index):]
             clause = re.sub('\'', '', clause)
             index = index[1:]
-            rhs += aggregation.join(_explicit_variables(VARIABLE_BASE_NAME + ('_' if int(index) + i < 0 else '') +
-                                    str(abs(int(index)) + i) + ' = ' + value.lower()) for i, value in enumerate(clause))
+            # TODO: compute next index with check on the original variables
+            rhs += aggregation.join(_explicit_variables(
+                VARIABLE_BASE_NAME + ('_' if _next_index(index, i) < 0 else '') + str(abs(_next_index(index, i))) +
+                ' = ' + value.lower()) for i, value in enumerate(clause))
         elif negation is not None:
             new_clause = re.sub(NOT_IDENTIFIER, NOT_SYMBOL, clause)
             new_clause = re.sub('-', '_', new_clause.lower())
@@ -69,7 +109,7 @@ def _parse_clause(rest: str, rhs: str = '', aggregation: str = AND_SYMBOL) -> st
             inner_clause = _parse_clause(new_clause, rhs, PLUS_SYMBOL)
             inner_clause = '(' + (')' + PLUS_SYMBOL + '(').join(e for e in inner_clause.split(PLUS_SYMBOL)) + ')'
             n = clause[n.regs[0][0]:n.regs[0][1] - 2]
-            rhs += n + LESS_SYMBOL + '(' + inner_clause + ')'
+            rhs += n + LESS_EQUAL_SYMBOL + '(' + inner_clause + ')'
         else:
             rhs += re.sub('-', '_', clause.lower()) + '()'
         if j < len(rest.split(',')) - 1:
@@ -77,10 +117,34 @@ def _parse_clause(rest: str, rhs: str = '', aggregation: str = AND_SYMBOL) -> st
     return rhs
 
 
+def _next_index(index: str, offset: int) -> int:
+    indices = get_indices()
+    new_index: int = int(index) + offset
+    modified: bool = False
+    while new_index not in indices:
+        new_index += 1
+        modified = True
+    return new_index + _previous_holes(indices, indices.index(new_index)) if not modified else new_index
+
+
+def _previous_holes(l: list[int], i: int) -> int:
+    j = 0
+    for k in list(range(0, i)):
+        if l[k] + 1 != l[k+1]:
+            j += 1
+    return j
+
+
 def _explicit_variables(e: str) -> str:
     result = ''
     for key in AGGREGATE_SYMBOLS.keys():
         if key.lower() in e:
             values = [v for v in ALPHABET if v not in AGGREGATE_SYMBOLS[key]]
-            result += AND_SYMBOL.join(NOT_SYMBOL + '(' + re.sub(key.lower(), value.lower(), e) + ')' for value in values)
+            result += AND_SYMBOL.join(
+                NOT_SYMBOL + '(' + re.sub(key.lower(), value.lower(), e) + ')' for value in values)
     return '(' + result + ')' if result != '' else e
+
+
+def _replace(s: str, e: str) -> str:
+    values = [v for v in AGGREGATE_DATA_SYMBOLS[s]]
+    return '(' + OR_SYMBOL.join('(' + re.sub(s.lower(), value.lower(), e) + ')' for value in values) + ')'
